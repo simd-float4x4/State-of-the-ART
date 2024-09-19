b20d7ecb5d37f1bd263f98aa7260276e6d1f770d
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-09-19 15:41:37 +0900

    fix: ポートフォリオ欄の全体的なインデント調整及び内容（ARFaceTacking）の追加とその他修正

--- src/components/Portfolio/PortfolioView.vue
+++ src/components/Portfolio/PortfolioView.vue
@@ -1,12 +1,13 @@
 <template>
     <h1 class="section-title">PORTFOLIO</h1>
     <div>
-        <PortfolioItemView title="2024 【Vue.js】STATE OF THE ART（ポートフォリオサイト）" URL="#"></PortfolioItemView>
-        <PortfolioItemView title="2024 【iOS/SwiftData】支払管理アプリ（仮称）" URL="https://github.com/simd-float4x4/Budget"></PortfolioItemView>
-        <PortfolioItemView title="2023-2024 【iOS/Android/SceneKit/FlutterxUnity】非公開プロジェクト"></PortfolioItemView>
-        <PortfolioItemView title="2021-2023 【iOS/ARKit/Swift/Blender】非公開プロジェクト URLは作例の一つ" URL="https://www.youtube.com/shorts/yoYT6ARBiNE"></PortfolioItemView>
-        <PortfolioItemView title="2019 【iOS】W -あなたの願いを叶えるWishList-（公開終了）" URL="https://appadvice.com/app/w-e3-81-82-e3-81-aa-e3-81-9f-e3-81-ae-e9-a1-98-e3-81-84-e3-82-92-e5-8f-b6-e3-81-88-e3-82-8bwish-list/1465571252"></PortfolioItemView>
-        <PortfolioItemView title="2019 【iOS】爽快脳トレ!! Plus10（公開終了）" URL="https://appadvice.com/game/app/e7-88-bd-e5-bf-ab-e8-84-b3-e3-83-88-e3-83-ac-plus10/1453404631"></PortfolioItemView>
+      <PortfolioItemView title="2024 【Vue.js】STATE OF THE ART（ポートフォリオサイト）" URL="#"></PortfolioItemView>
+      <PortfolioItemView title="2024 【iOS/SwiftData】支払管理アプリ（仮称）" URL="https://github.com/simd-float4x4/Budget"></PortfolioItemView>
+      <PortfolioItemView title="2022　【iOS／ARKit／Blender】ARFaceTrackingを用いたアプリケーション" URL="https://www.youtube.com/shorts/iki9E3-7z4Q"></PortfolioItemView>
+      <PortfolioItemView title="2021-2023 【iOS/ARKit/Swift/Blender】非公開プロジェクト URLは作例の一つ" URL="https://www.youtube.com/shorts/yoYT6ARBiNE"></PortfolioItemView>
+      <PortfolioItemView title="2023-2024 【iOS/Android/SceneKit/FlutterxUnity】非公開プロジェクト"></PortfolioItemView>
+      <PortfolioItemView title="2019 【iOS】W -あなたの願いを叶えるWishList-（公開終了）" URL="https://appadvice.com/app/w-e3-81-82-e3-81-aa-e3-81-9f-e3-81-ae-e9-a1-98-e3-81-84-e3-82-92-e5-8f-b6-e3-81-88-e3-82-8bwish-list/1465571252"></PortfolioItemView>
+      <PortfolioItemView title="2019 【iOS】爽快脳トレ!! Plus10（公開終了）" URL="https://appadvice.com/game/app/e7-88-bd-e5-bf-ab-e8-84-b3-e3-83-88-e3-83-ac-plus10/1453404631"></PortfolioItemView>
     </div>
 </template>
 
