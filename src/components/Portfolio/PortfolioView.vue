af6dfdedc0ab65acbe933a77040ec6264cd0b7bf
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-09-19 16:00:40 +0900

    fix: Irregular whitespace not allowed  no-irregular-whitespace対策

--- src/components/Portfolio/PortfolioView.vue
+++ src/components/Portfolio/PortfolioView.vue
@@ -3,7 +3,7 @@
     <div>
       <PortfolioItemView title="2024 【Vue.js】STATE OF THE ART（ポートフォリオサイト）" URL="#"></PortfolioItemView>
       <PortfolioItemView title="2024 【iOS/SwiftData】支払管理アプリ（仮称）" URL="https://github.com/simd-float4x4/Budget"></PortfolioItemView>
-      <PortfolioItemView title="2022　【iOS／ARKit／Blender】ARFaceTrackingを用いたアプリケーション" URL="https://www.youtube.com/shorts/iki9E3-7z4Q"></PortfolioItemView>
+      <PortfolioItemView title="2022 【iOS／ARKit／Blender】ARFaceTrackingを用いたアプリケーション" URL="https://www.youtube.com/shorts/iki9E3-7z4Q"></PortfolioItemView>
       <PortfolioItemView title="2021-2023 【iOS/ARKit/Swift/Blender】非公開プロジェクト URLは作例の一つ" URL="https://www.youtube.com/shorts/yoYT6ARBiNE"></PortfolioItemView>
       <PortfolioItemView title="2023-2024 【iOS/Android/SceneKit/FlutterxUnity】非公開プロジェクト"></PortfolioItemView>
       <PortfolioItemView title="2019 【iOS】W -あなたの願いを叶えるWishList-（公開終了）" URL="https://appadvice.com/app/w-e3-81-82-e3-81-aa-e3-81-9f-e3-81-ae-e9-a1-98-e3-81-84-e3-82-92-e5-8f-b6-e3-81-88-e3-82-8bwish-list/1465571252"></PortfolioItemView>
